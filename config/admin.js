3dbd08c5a1751c8dc646b8a877d524e18531b0dd
Vignesh Sekar <vignesh.seker@aceolution.com>
2025-11-21 12:24:11 +0530

    fix: use postgres on railway

--- config/admin.js
+++ config/admin.js
@@ -1,11 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
-
-    sessions: {
-      maxRefreshTokenLifespan: env("ADMIN_MAX_REFRESH_TOKEN_LIFESPAN", "30d"),
-      maxSessionLifespan: env("ADMIN_MAX_SESSION_LIFESPAN", "30d"),
-    },
   },
   apiToken: {
     salt: env("API_TOKEN_SALT"),
